bcbcc3062358aa4d8085363ec83b6c3049c81b8a
Nastya Ovsiy <no.korolevaa@gmail.com>
2017-02-22 19:36:54 +0200

    Add TestController

---
+++ Novsiy.Web/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Novsiy.Web.Models.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Novsiy.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            var model = new TestIndexViewModel();
+            model.SomeValue = 10;
+
+
+            return View(model);
+        }
+    }
+}
---
+++ Novsiy.Web/Models/Test/TestIndexViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Novsiy.Web.Models.Test
+{
+    public class TestIndexViewModel
+    {
+        public int SomeValue { get; set; }
+    }
+}
